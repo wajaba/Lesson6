30d3045b48ad7e9009dcf35af75c8ecb2f001d47
tann1260 <tann1260@ISS147D8-27.scdsb.pri>
2014-10-07 08:50:42 -0400

    Final piece, running bug tests.

--- src/MakeEmployee.java
+++ src/MakeEmployee.java
@@ -16,9 +16,45 @@ public class MakeEmployee {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        KeyboardReader kr = new KeyboardReader();
-        String name = kr.readLine("enter name");
-        System.out.println(name);
+        KeyboardReader r = new KeyboardReader();
+        Employee emp;
+        String name;
+        int type, hours;
+        double rate;
+        String prompt;
+        emp = new Employee();
+        while (true) {
+            System.out.println("Enter employee data");
+            name = r.readLine(" Name(or <enter> to quit): ");
+            if (!(emp.setName(name))) {
+                break;
+            }
+
+            while (true) {
+                prompt = " type (" + emp.getTypeRules() + "): ";
+                type = r.readInt(prompt);
+                if (emp.setType(type)) {
+                    break;
+                }
+            }
+            while (true) {
+                prompt = "hours(" + emp.getHourRules() + "): ";
+                hours = r.readInt(prompt);
+                if (emp.sethours(hours)) {
+                    break;
+                }
+            }
+            while (true) {
+                prompt = " Hourly rate(" + emp.getRateRules() + "): ";
+                rate = r.readDouble(prompt);
+                if (emp.setRate(rate)) {
+                    break;
+                }
+            }
+            System.out.print(" the weekly pay for ");
+            System.out.print(emp.getName());
+            System.out.println(" is $" + emp.getPay());
+        }
     }
 
 }
