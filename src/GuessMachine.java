de6c70399ddbe5f5bebdb52dc9d8d12a432a9d57
tann1260 <tann1260@ISS147D8-27.scdsb.pri>
2014-10-08 09:03:05 -0400

    Final piece, running bug tests.

--- src/GuessMachine.java
+++ src/GuessMachine.java
@@ -1,22 +1,35 @@
+
 public class GuessMachine {
-    int number, numguess,plyrguess;
-    
-    public GuessMachine(){
-    number =0;
-    numguess = 0;
-    plyrguess = 0;
-}
-    public double getNumber() {
-       return Math.random()*100 +1;
+
+    int number, numguess, plyrguess;
+
+    public GuessMachine() {
+        number = (int) (Math.random() * 100) + 1;
+        numguess = 0;
+        plyrguess = 0;
     }
+
     public boolean CheckGuess(int G) {
         boolean Guess = (G >= 1 && G <= 100);
         if (Guess) {
             plyrguess = G;
+            numguess++;
             return true;
         } else {
 
             return false;
         }
     }
+
+    public String giveHint(String H) {
+        if (plyrguess == number) {
+            return "you got it";
+        } else if (plyrguess >= number) {
+            return "too high";
+        } else {
+            return "too low";
+        }
+
+    }
+
 }
